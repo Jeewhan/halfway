2f2de62af31675edea6056868078d4849684476e
Jeewhan <distinct19@gmail.com>
2018-02-02 22:01:07 +0900

    Edit every test code

--- __tests__/every.test.js
+++ __tests__/every.test.js
@@ -1,4 +1,9 @@
 import every from "../every";
+import filter from "../filter";
+import contains from "../contains";
+import go from "../go";
+import first from "../first";
+import select from "../select";
 
 describe("every", () => {
   it(`[2, 4, 5] + num % 1 === 0`, () => {
@@ -24,5 +29,45 @@ describe("every", () => {
 
     // then
     expect(result).toEqual(false);
+  });
+
+  // https://github.com/joeunha/functional-js-study2/issues/5
+  // 도시(city) / 나이(age) / 혈액형(blood)을 배열로 넣으면 해당하는 사람을 찾는 user_filter 함수 완성하기
+  it(`user_filter function`, () => {
+    // given
+    const users = [
+      { name: "ID", age: 32, city: "seoul", blood: "A" },
+      { name: "BJ", age: 31, city: "seoul", blood: "O" },
+      { name: "JM", age: 32, city: "busan", blood: "B" },
+      { name: "PJ", age: 27, city: "seongnam", blood: "B" },
+      { name: "HA", age: 27, city: "seoul", blood: "O" },
+      { name: "JE", age: 27, city: "seongnam", blood: "O" },
+      { name: "JI", age: 32, city: "seongnam", blood: "A" },
+      { name: "MP", age: 28, city: "busan", blood: "O" },
+      { name: "JY", age: 31, city: "seoul", blood: "AB" },
+      { name: "TH", age: 27, city: "busan", blood: "O" }
+    ];
+    const cities = ["busan", "seoul"];
+    const ages = [32];
+    const bloods = ["B"];
+    const user_filter = (cities, ages, bloods) => given =>
+      filter(given, ({ city, age, blood }) =>
+        every([
+          contains(cities, city),
+          contains(ages, age),
+          contains(bloods, blood)
+        ])
+      );
+
+    // when
+    const result = go(
+      users,
+      user_filter(cities, ages, bloods),
+      first,
+      select("name")
+    );
+
+    // then
+    expect(result).toEqual("JM");
   });
 });
