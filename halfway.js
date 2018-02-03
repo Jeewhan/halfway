2549cf7150e7951ddf6681b3b0ae2e7a6fb9d40e
Jeewhan <distinct19@gmail.com>
2018-02-03 11:32:59 +0900

    Add export functions

--- halfway.js
+++ halfway.js
@@ -3,7 +3,25 @@ const map = require("./map");
 const reduce = require("./reduce");
 const select = require("./select");
 const contains = require("./contains");
+const every = require("./every");
+const callr = require("./callr");
+const filter = require("./filter");
+const curryr = require("./curryr");
+const first = require("./first");
+const go = require("./go");
 
-const H = { each, map, reduce, select, contains };
+const H = {
+  each,
+  map,
+  reduce,
+  select,
+  contains,
+  every,
+  callr,
+  filter,
+  curryr,
+  first,
+  go
+};
 
 module.exports = H;
