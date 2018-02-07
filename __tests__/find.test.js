3f04703a8a7464eb03daf121b3f21383c4308083
Jeewhan <distinct19@gmail.com>
2018-02-07 22:49:49 +0900

    find

---
+++ __tests__/find.test.js
@@ -0,0 +1,49 @@
+import find from "../find";
+
+describe(`find`, () => {
+  // http://underscorejs.org/#find
+  it(`[1, 2, 3, 4, 5, 6] + num => num % 2 === 0`, () => {
+    // given
+    const given = [1, 2, 3, 4, 5, 6];
+    const func = num => num % 2 === 0;
+
+    // when
+    const result = find(given, func);
+
+    // then
+    expect(result).toEqual(2);
+  });
+
+  let users;
+
+  beforeEach(() => {
+    users = [
+      { user: "barney", age: 36, active: true },
+      { user: "fred", age: 40, active: false },
+      { user: "pebbles", age: 1, active: true }
+    ];
+  });
+
+  // https://lodash.com/docs/4.17.4#find
+  it(`Array + ({ age }) => age < 40`, () => {
+    // given
+    // users
+
+    // when
+    const result1 = find(users, ({ age }) => age < 40);
+
+    // then
+    expect(result1).toEqual({ user: "barney", age: 36, active: true });
+  });
+
+  it(`Array + Object`, () => {
+    // given
+    // users
+
+    // when
+    const result2 = find(users, { age: 1, active: true });
+
+    // then
+    expect(result2).toEqual({ user: "pebbles", age: 1, active: true });
+  });
+});
---
+++ find.js
@@ -0,0 +1,16 @@
+const matcher = require("./matcher");
+
+function find(collection, predicate, fromIndex) {
+  let i = fromIndex - 1 || -1;
+  const len = collection.length;
+
+  if (typeof predicate === "function") {
+    while (++i < len)
+      if (predicate(collection[i], i, collection)) return collection[i];
+  } else if (predicate instanceof Object) {
+    const match = matcher(predicate);
+    while (++i < len) if (match(collection[i])) return collection[i];
+  }
+}
+
+module.exports = find;
