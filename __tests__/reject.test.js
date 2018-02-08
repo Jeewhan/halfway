748c6fa29f14db123fb1f4eb1d322d812c10e54a
Jeewhan <distinct19@gmail.com>
2018-02-09 01:37:32 +0900

    reject

---
+++ __tests__/reject.test.js
@@ -0,0 +1,15 @@
+import reject from "../reject";
+
+// http://underscorejs.org/#filter
+describe(`reject`, () => {
+  it(`[1, 2, 3, 4, 5, 6] + num => num % 2 === 0`, () => {
+    // given
+    const given = [1, 2, 3, 4, 5, 6];
+
+    // when
+    const result = reject(given, num => num % 2 === 0);
+
+    // then
+    expect(result).toEqual([1, 3, 5]);
+  });
+});
---
+++ reject.js
@@ -0,0 +1,11 @@
+function reject(collection, predicate) {
+  let i = -1;
+  const len = collection.length;
+  const result = [];
+
+  while (++i < len) if (!predicate(collection[i])) result.push(collection[i]);
+
+  return result;
+}
+
+module.exports = reject;
