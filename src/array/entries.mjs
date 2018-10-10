de95a253dc1f39e3c6c6f8d16ec3edb1a642c43d
Jeewhan <distinct19@gmail.com>
2018-10-11 05:03:24 +0900

    Edit entries

--- src/array/entries.mjs
+++ src/array/entries.mjs
@@ -1,7 +1,7 @@
 import reduce from "../collection/reduce.mjs";
 
 function entries(list) {
-  return reduce(list, (acc, v, i) => acc.concat([[v, i]]), []);
+  return reduce(list, (acc, v, i) => acc.concat([[i, v]]), []);
 }
 
 export default entries;
--- src/array/entries.test.js
+++ src/array/entries.test.js
@@ -9,6 +9,19 @@ describe("entries", () => {
     const result = entries(given)[Symbol.iterator]();
 
     // then
+    expect(result.next().value).toEqual([0, "a"]);
+    expect(result.next().value).toEqual([1, "b"]);
+    expect(result.next().value).toEqual([2, "c"]);
+  });
+
+  test(`{ a: 0, b: 1, c: 2 }`, () => {
+    // given
+    const given = { a: 0, b: 1, c: 2 };
+
+    // when
+    const result = entries(given)[Symbol.iterator]();
+
+    // then
     expect(result.next().value).toEqual(["a", 0]);
     expect(result.next().value).toEqual(["b", 1]);
     expect(result.next().value).toEqual(["c", 2]);
