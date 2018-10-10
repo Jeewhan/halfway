2eb95d06bc9f16d3d02c53951bb574a79439696f
Jeewhan <distinct19@gmail.com>
2018-10-11 02:52:29 +0900

    Edit matcher

--- src/object/matcher.mjs
+++ src/object/matcher.mjs
@@ -1,10 +1,12 @@
+import keys from "../utility/keys.mjs";
+
 function matcher(attributes) {
   let i = -1;
-  const keys = Object.keys(attributes);
-  const len = keys.length;
+  const ks = keys(attributes);
+  const len = ks.length;
 
   return obj => {
-    while (++i < len) if (obj[keys[i]] !== attributes[keys[i]]) return false;
+    while (++i < len) if (obj[ks[i]] !== attributes[ks[i]]) return false;
     return true;
   };
 }
